b382e7ebd2f60b4ef5d6f6e6dbaac2e2df979869
dctalbot <david.talbot@target.com>
2022-02-21 16:19:20 -0600

    fmt

--- web/explore/explore.tsx
+++ web/explore/explore.tsx
@@ -3,26 +3,15 @@ import { render } from 'react-dom'
 import { GraphiQL } from 'graphiql'
 import 'graphiql/graphiql.css'
 
-// const Logo = function Logo(): JSX.Element {
-//   return <span>My Corp</span>
-// }
-
-// Logo.displayName = 'logo'
-
-// // See GraphiQL Readme - Advanced Usage section for more examples like this
-// GraphiQL.Logo = Logo
-
-const App = (): JSX.Element => { 
+const App = (): JSX.Element => {
   const path = location.host + location.pathname.replace(/\/explore.*$/, '')
 
-
-  return <div style={{ height: '100vh' }}>
-    <GraphiQL
-      headerEditorEnabled
-      fetcher={async (graphQLParams) => {
-        const resp = await fetch(
-          location.protocol + '//' + path,
-          {
+  return (
+    <div style={{ height: '100vh' }}>
+      <GraphiQL
+        headerEditorEnabled
+        fetcher={async (graphQLParams) => {
+          const resp = await fetch(location.protocol + '//' + path, {
             method: 'POST',
             headers: {
               Accept: 'application/json',
@@ -30,12 +19,12 @@ const App = (): JSX.Element => {
             },
             body: JSON.stringify(graphQLParams),
             credentials: 'same-origin',
-          },
-        )
-        return resp.json().catch(() => resp.text())
-      }}
-    />
-  </div>
+          })
+          return resp.json().catch(() => resp.text())
+        }}
+      />
+    </div>
+  )
 }
 
 render(<App />, document.getElementById('root'))
