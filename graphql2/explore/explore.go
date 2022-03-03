770ca340450da651744342739202bd0a83c0befc
dctalbot <david.talbot@target.com>
2022-03-03 12:38:58 -0600

    use abs path

--- graphql2/explore/explore.go
+++ graphql2/explore/explore.go
@@ -5,17 +5,20 @@ import (
 	_ "embed"
 	"html/template"
 	"net/http"
+	"path/filepath"
 
+	"github.com/pkg/errors"
 	"github.com/target/goalert/config"
 	"github.com/target/goalert/permission"
 	"github.com/target/goalert/util/errutil"
+	"github.com/target/goalert/util/log"
 )
 
 //go:embed explore.html
 var htmlStr string
 
 //go:embed build
-var build embed.FS
+var fs embed.FS
 
 var playTmpl = template.Must(template.New("graphqlPlayground").Parse(htmlStr))
 
@@ -32,12 +35,25 @@ func Handler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	jsData, err := build.ReadFile("build/explore/explore.js")
+	jspath, err := filepath.Abs(filepath.Join("graphql2", "explore", "build", "explore.js"))
 	if err != nil {
+		log.Log(ctx, errors.Wrap(err, "graphql explore js path"))
 		return
 	}
-	cssData, err := build.ReadFile("build/explore/explore.css")
+	csspath, err := filepath.Abs(filepath.Join("graphql2", "explore", "build", "explore.css"))
 	if err != nil {
+		log.Log(ctx, errors.Wrap(err, "graphql explore css path"))
+		return
+	}
+
+	jsData, err := fs.ReadFile(jspath)
+	if err != nil {
+		log.Log(ctx, errors.Wrap(err, "read graphql explore js"))
+		return
+	}
+	cssData, err := fs.ReadFile(csspath)
+	if err != nil {
+		log.Log(ctx, errors.Wrap(err, "read graphql explore css"))
 		return
 	}
 
